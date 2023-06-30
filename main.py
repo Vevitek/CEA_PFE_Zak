c15f70f517edd2921f6131897426effa484942fb
Zak_ariya <aitathmane.zakariya@gmail.com>
2023-06-30 14:20:35 +0200

    Brief description of how to proceed

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from MSDtraj import *
 
 filename = r'D:\...' #exact location of your file, for example r'E:\...\my_file.csv
 
-pathfile = r'S:\Data_Zak\Fiji files\12 06 2023' #path where you want to register your data (images + text files for MSD calculation)
+pathfile = r'S:\...' #path where you want to register your data (images + text files for MSD calculation)
 name_file = r'\prefix of your file' #make sure you keep the "\" before the name you want to give to your files
 
 
 ##---------------------Image processing---------------------##
 
 # Rep_traj_unchanged(filename,pathfile)
-# plt.savefig(pathfile+name_file+'_traj_unchanged.png')
+plt.savefig(pathfile+name_file+'_traj_unchanged.png')
 # Rep_same_origin(filename)
-# plt.savefig(pathfile+name_file+'_same_origin.png')
+plt.savefig(pathfile+name_file+'_same_origin.png')
 # Distrib_direction_hist(filename)
-# plt.savefig(pathfile+name_file+'_hist.png')
+plt.savefig(pathfile+name_file+'_hist.png')
 
 ##---------------------MSD calculation---------------------##
 msd = MSDtraj(pathfile,['t','x','y'],50,0)   # MSDtraj(pathfile, [time, x_position, y_position],
